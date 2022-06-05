67a07d8fc10d04f20e15aaa3facf7c5e44735008
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2022-06-05 12:10:04 +0530

    Committing files

---
+++ CodeChef/C++14/HELCARP/34339064.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define MOD 1000000007
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+using namespace std::chrono;
+
+ 
+int main() {
+    fastIO;
+    int t; cin >> t;
+    for(int qq = 0; qq < t; qq++) {
+        long int n, k;
+        cin >> n >> k;
+        long long int l[k+1], d[k+1];
+        for(long int i = 1; i <= k; i++)
+            cin >> l[i];
+        for(long int i = 1; i <= k; i++)
+            cin >> d[i];
+        ll dp[k+1][n+1];
+        memset(dp, 0, sizeof(dp));
+        dp[0][0] = 1; // if you need to get 0 logs out of 0 types of bundles, only way is to DO NOTHING
+        for(ll i = 1; i <= k; i++) {
+            for(ll j = 0; j <= n; j++) {
+                ll rem = min(d[i], j/l[i]);
+                for(ll f = 0; f <= rem; f++)
+                    dp[i][j] = (dp[i][j]%MOD + dp[i-1][j-l[i]*f]%MOD) %MOD;
+            }
+        }
+        cout << dp[k][n]%MOD << endl;
+    }
+
+} 
