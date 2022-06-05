fdc0735e8241f454a7a7dd2812f2feb157ff8591
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2022-06-05 11:58:21 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc142_b/17195607.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long int
+#define ll long long
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
+#define INF 1e18
+/*#define Time cerr<<"\nTime Taken : "<<(float)(clock()-time_p)/CLOCKS_PER_SEC<<"\n";
+clock_t time_p=clock();*/
+int main(){
+    fast_io
+    #ifndef ONLINE_JUDGE
+    freopen("inputa.txt","r",stdin);
+    freopen("outputa.txt","w",stdout);
+    freopen("log.txt","w",stderr);
+    #endif
+ll n;
+cin>>n;
+if(n%2==0){
+    cout<<fixed<<setprecision(7)<<0.5<<endl;
+}
+else
+{
+    double ans = (n/2+1)/(double (n));
+ cout<<fixed<<setprecision(7)<<ans<<endl;
+}
+}
+
