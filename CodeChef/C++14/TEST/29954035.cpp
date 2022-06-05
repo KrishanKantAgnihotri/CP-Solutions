ca775dd6ef61791e6ab2daf6caceda4f7c839a06
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2022-06-05 12:11:26 +0530

    Committing files

---
+++ CodeChef/C++14/TEST/29954035.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+//#include<boost/multiprecision/cpp_int.hpp>
+//using boost::multiprecision::cpp_int;
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	while(n!=42)
+	{cout<<n<<endl;
+	cin>>n;
+	}
+	return 0;
+}
