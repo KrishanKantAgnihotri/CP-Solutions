47844a2ff27142c52755d76040c57f3364638645
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2022-06-05 12:11:51 +0530

    Committing files

---
+++ CodeChef/C++14/FLOW004/29934781.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+                int t;
+                cin>>t;
+                while(t--){
+                string s;
+                cin>>s;
+                int n;
+                n = s.length();
+                int sum = (s[0]-'0')+s[n-1]-'0';
+                cout<<sum<<"\n";
+                }
+            
+	return 0;
+}
