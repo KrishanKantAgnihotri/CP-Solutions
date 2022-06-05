0f352356f82a8b4f1bfd6b723f484f52d285f5db
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2022-06-05 12:04:11 +0530

    Committing files

---
+++ CodeChef/C++14/SIFTING/30948426.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+#include<boost/multiprecision/cpp_int.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+#define ull unsigned long long int
+#define ll long long
+#define vi vector<int>
+#define mii map<int,int>
+#define all(x) x.begin(),x.end()
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
+#define pb push_back
+#define MOD 1e9+7
+#define INF 0x3f3f3f3f
+//#define show(x) cerr<<x#<<" "<<x<<"\n";
+
+//typedef tree<pair<int,int>,null_type, 
+//less<pair<int,int>>,rb_tree_tag,tree_order_statistics_node_update>   ordered_set;
+
+//(UNCOMMENT WHEN HAVING LOTS OF RECURSIONS)\
+#pragma comment(linker, "/stack:200000000")
+//(UNCOMMENT WHEN NEEDED)\
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+//#pragma GCC optimize("O3")
+//uncoment for large int requirement
+
+//using boost::multiprecision::cpp_int;
+//using namespace _gnu_pbds;
+using namespace std;
+
+bool isprime(ull n)
+{
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+    if(n%2 == 0 || n%3 == 0) return false;
+
+    for (int i=5; i*i<=n; i=i+6)
+        if (n%i == 0 || n%(i+2) == 0)
+           return false;
+    return true;
+}
+int main()
+{fast_io
+#ifndef ONLINE_JUDGE
+freopen("in.txt","r",stdin);
+freopen("out.txt","w",stdout);
+#endif
+int number;
+cin>>number;
+int cnt=0;
+for(int i = 2 ; i<=number;i++)
+{
+    if(isprime(i))
+    cnt++;
+}
+ cout<<cnt<<endl;
+}
