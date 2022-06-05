114473fa274f74ac2b32127fecb0e5fc93176c54
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2022-06-05 11:59:20 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc168_a/14698357.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+//#include<boost/multiprecision/cpp_int.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+#define ull unsigned long long int
+#define ll long long
+#define vi vector<int>
+#define mii map<int,int>
+#define all(x) x.begin(),x.end()
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
+#define pb push_back
+#define MOD 1e9+7
+#define INF 0x3f3f3f3f
+//#define show(x) cerr<<x#<<" "<<x<<"\n";
+
+//typedef tree<pair<int,int>,null_type, 
+//less<pair<int,int>>,rb_tree_tag,tree_order_statistics_node_update>   ordered_set;
+
+//(UNCOMMENT WHEN HAVING LOTS OF RECURSIONS)\
+#pragma comment(linker, "/stack:200000000")
+//(UNCOMMENT WHEN NEEDED)\
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+//#pragma GCC optimize("O3")
+//uncoment for large int requirement
+
+//using boost::multiprecision::cpp_int;
+//using namespace _gnu_pbds;
+using namespace std;
+int dx[]={-1,0,1,0};
+int dy[]={0,1,0,-1};
+
+int main()
+{
+ll n;
+cin>>n;
+ll ans=(n/1000+1)*1000-n;
+cout<<ans%1000<<endl;
+}
