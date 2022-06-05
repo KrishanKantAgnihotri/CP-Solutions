25de242dacc3a9389f7f34b623a8ac5174d8fa70
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2022-06-05 11:59:47 +0530

    Committing files

---
+++ Atcoder/C++ (Clang 10.0.0)/abc167_b/13150384.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b,c,k;
+    cin>>a>>b>>c>>k;
+  if(k<=a)
+    cout<<k<<endl;
+  else
+    if(k<=a+b)
+      cout<<a<<endl;
+  else 
+    cout<<a-(k-(a+b))<<endl;
+}
