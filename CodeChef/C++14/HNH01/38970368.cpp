6b32c6a14710e5e3f88b55091afea6e4e4be4c4b
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2022-06-05 12:08:16 +0530

    Committing files

---
+++ CodeChef/C++14/HNH01/38970368.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long int
+#define ll long long
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
+#define INF 1e18
+#define Time cerr<<"\nTime Taken : "<<(float)(clock()-time_p)/CLOCKS_PER_SEC<<"\n";
+clock_t time_p=clock();
+int main(){
+    fast_io
+    #ifndef ONLINE_JUDGE
+    freopen("inputa.txt","r",stdin);
+    freopen("outputa.txt","w",stdout);
+    freopen("log.txt","w",stderr);
+    #endif
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        cout<<s.length()<<endl;
+    }
+Time
+}
+
