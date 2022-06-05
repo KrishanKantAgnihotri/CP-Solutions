c8a92723cfae0932198c704e868b5cca10b7857b
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2022-06-05 12:10:46 +0530

    Committing files

---
+++ CodeChef/C++14/CHFDORA/30756877.cpp
@@ -0,0 +1,89 @@
+#include<bits/stdc++.h>
+//#include<boost/multiprecision/cpp_int.hpp>
+#define ull unsigned long long int
+#define ll long long
+#define vi vector<int>
+#define pi pair<int,int>
+#define vii vector<pi>
+#define eb emplace_back
+#define mii map<int,int>
+#define all(x) x.begin(),x.end()
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define rep(i,a,b) for(ll i=a ;i<=b ; i++)
+#define endl "\n"
+#define pb push_back
+#define mp make_pair
+#define MOD 1000000007
+#pragma GCC optimize("O3")
+#define gcd __gcd(a,b)
+#define lcm(a,b) (((a)*(b))/__gcd(a,b))
+#define cntb(a) __builtin_popcount(a)
+#define show(x) cout<<x<<" "
+//(UNCOMMENT WHEN HAVING LOTS OF RECURSIONS)
+#pragma comment(linker, "/stack:200000000")
+//(UNCOMMENT WHEN NEEDED)
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+
+using namespace std;
+/*bool isprime(ull n) 
+{ 
+    if (n <= 1) return false; 
+    if (n <= 3) return true; 
+    if(n%2 == 0 || n%3 == 0) return false;
+
+    for (int i=5; i*i<=n; i=i+6) 
+        if (n%i == 0 || n%(i+2) == 0) 
+           return false; 
+    return true; 
+}
+ll ipow(ll base, ll exp)
+{
+    ll result = 1;
+    for (;;)
+    {
+        if (exp & 1)
+            result *= base;
+        exp >>= 1;
+        if (!exp)
+            break;
+        base *= base;
+    }
+
+    return result;
+}*/
+
+int main()
+{fast_io
+int t;
+cin>>t;
+while(t--)
+{ll n,m;
+cin>>n>>m;
+int a[n][m];
+ll cnt = 0 ; 
+
+
+for(int i = 0 ; i<n  ;i++)
+for(int j= 0 ; j<m ; j++)
+cin>>a[i][j];
+cnt+=(n*m);ll tmp = 0 ;
+for(int i = 1 ;i<n-1;i++)
+{
+    for(int j = 1 ; j<m-1; j++)
+    {for(int k = 1; ((i+k<n)&&(i-k>=0))&&(((j+k<m)&&(j-k>=0)));k++)
+       {//cout<<a[i+k][j]<<" "<<a[i-k][j]<<"  "<<a[i][j-k]<<" "<<a[i][j+k]<<endl;
+       if((a[i+k][j]==a[i-k][j])&&(a[i][j+k]==a[i][j-k]))
+         tmp++;
+         else
+         break;
+     }
+       }
+    }
+
+
+cnt+=tmp;
+cout<<cnt<<endl;
+}
+}
