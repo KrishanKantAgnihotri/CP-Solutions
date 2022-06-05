0021996dd0e32738ad99949a7d7829402680b121
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2022-06-05 11:59:17 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc172_b/14732357.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+
+#define ull unsigned long long int
+#define ll long long
+#define vi vector<int>
+#define mii map<int,int>
+#define all(x) x.begin(),x.end()
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
+#define pb push_back
+#define MOD 1e9+7
+#define INF 0x3f3f3f3f
+
+using namespace std;
+int dx[]={-1,0,1,0};
+int dy[]={0,1,0,-1};
+
+int main()
+{
+ll n;
+cin>>n;
+ll ans =n+n*n+n*n*n;
+cout<<ans<<endl;
+}
