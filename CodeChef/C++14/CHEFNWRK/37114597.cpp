8d264b11620d94b8dc35827b35ff812884952b2a
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2022-06-05 12:08:45 +0530

    Committing files

---
+++ CodeChef/C++14/CHEFNWRK/37114597.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+//#include<boost/multiprecision/cpp_int.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+#define ull unsigned long long int
+#define ll long long
+#define vi vector<int>
+#define mii map<int,int>
+#define all(x) x.begin(),x.end()
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
+#define pb push_back
+#define MOD 1e9+7
+#define INF 0x3f3f3f3f
+//#define show(x) cerr<<x#<<" "<<x<<"\n";
+
+//typedef tree<pair<int,int>,null_type, 
+//less<pair<int,int>>,rb_tree_tag,tree_order_statistics_node_update>   ordered_set;
+
+//(UNCOMMENT WHEN HAVING LOTS OF RECURSIONS)\
+#pragma comment(linker, "/stack:200000000")
+//(UNCOMMENT WHEN NEEDED)\
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+//#pragma GCC optimize("O3")
+//uncoment for large int requirement
+
+//using boost::multiprecision::cpp_int;
+//using namespace _gnu_pbds;
+using namespace std;
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+//use it with unordered_map<T,T,custom_hash> safe_map
+int dx[]={-1,0,1,0};
+int dy[]={0,1,0,-1};
+
+int main()
+{fast_io
+#ifndef ONLINE_JUDGE
+freopen("in.txt","r",stdin);
+freopen("out.txt","w",stdout);
+#endif
+int t;
+cin>>t;
+while(t--)
+{
+ ll n,k;
+ cin>>n>>k;
+ vi v(n);
+ for(int i = 0 ;i<n ;i ++)
+ cin>>v[i];
+ bool ok = true;
+ ll sum = 0 ; 
+ int cnt = 0 ; 
+ for(int i = 0 ;i<n ;i++){
+     if(sum+v[i]<=k){
+         sum+=v[i];
+     }
+     else
+     {if(sum==0)
+      {
+          ok = false;
+          break;
+      }
+         cnt++;
+         if(v[i]<=k)
+         sum=v[i];
+         else
+         {
+             ok = false;
+             break;
+         }
+     }
+ }
+if(ok)
+    cout<<cnt+1<<endl;
+    else
+    cout<<-1<<endl;
+}
+}
