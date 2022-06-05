972e0fb46113019a84f755d3d3cd7421bfe3765b
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2022-06-05 11:59:35 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc170_b/14485736.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+//#include<boost/multiprecision/cpp_int.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+#define ull unsigned long long int
+#define ll long long
+#define vi vector<int>
+#define mii map<int,int>
+#define all(x) x.begin(),x.end()
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
+#define pb push_back
+#define MOD 1e9+7
+#define INF 0x3f3f3f3f
+using namespace std;
+int dx[]={-1,0,1,0};
+int dy[]={0,1,0,-1};
+
+int main()
+{
+int a,b,c,d,e;
+cin>>a>>b>>c>>d>>e;
+if(a==0)
+cout<<1<<endl;
+if(b==0)
+cout<<2<<endl;
+if(c==0)
+cout<<3<<endl;
+if(d==0)
+cout<<4<<endl;if(e==0)
+cout<<5<<endl;
+}
