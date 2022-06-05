24421f3e3b45fa4d150dd32d8a4de3a37d8f9571
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2022-06-05 12:00:46 +0530

    Committing files

---
+++ CodeChef/C++14/STFOOD/31368204.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+//#include<boost/multiprecision/cpp_int.hpp>
+//using boost::multiprecision::cpp_int;
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{int j ; int num;
+	int n;
+	cin>>n;
+int s,p,v;
+int ans = 0 ; 
+for(int i = 0 ; i<n ; i++)
+{
+cin>>s>>p>>v;
+ans = max(ans,(p/(s+1))*v);
+	}
+	cout<<ans<<endl;}
+	return 0;
+}
