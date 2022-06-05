f0e0409e478056f1b3e030b1f025ea80a12a49b9
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2022-06-05 11:59:43 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc169_b/13954104.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+#define ull unsigned long long int
+#define ll long long
+#define vi vector<int>
+#define mii map<int,int>
+#define all(x) x.begin(),x.end()
+#define endl "\n"
+#define pb push_back
+#define MOD 1e9+7
+#define INF 0x3f3f3f3f
+
+using namespace std;
+int dx[]={-1,0,1,0};
+int dy[]={0,1,0,-1};
+
+int main()
+{
+int a,b;
+  cin>>a>>b;
+  cout<<a*b<<endl;
+
+}
