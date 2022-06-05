06f9da675a4f57fc89b96adec1ca8c7af405038c
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2022-06-05 11:56:37 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/practice2_a/20999755.cpp
@@ -0,0 +1,126 @@
+         
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long int
+#define ll long long
+#define ii pair<int,int>
+#define vii vector<ii>
+#define vi vector<int>
+#define vl vector<ll>
+#define mii map<int,int>
+#define uii unordered_map<int,int>
+#define all(x) x.begin(),x.end()
+#define ff first
+#define fr(i,a,b) for(int i= a ;i<=b ;i++)
+#define ss second
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
+#define pb push_back
+#define INF 1e18
+#define lcm(a,b) a*b/__gcd(a,b)
+#define print(x) cout<<x<<"\n";
+#define scanv(v,n) for(int i  = 0 ; i<n ;i++ ) cin>>v[i];
+#define printv(v) for(auto it : v) cout<<it<<" ";
+#define rall(v) v.rbegin(),v.rend()
+#define GOOGLE(i) cout<<"Case"<<" #"<<i<<": ";
+#define Time cerr<<"\nTime Taken : "<<(float)(clock()-time_p)/CLOCKS_PER_SEC<<"\n";
+clock_t time_p=clock();
+        // Implement (union by size) + (path compression)
+// Reference:
+// Zvi Galil and Giuseppe F. Italiano,
+// Data structures and algorithms for disjoint set union problems
+struct dsu {
+  public:
+    dsu() : _n(0) {}
+    dsu(int n) : _n(n), parent_or_size(n, -1) {}
+
+    int merge(int a, int b) {
+        assert(0 <= a && a < _n);
+        assert(0 <= b && b < _n);
+        int x = leader(a), y = leader(b);
+        if (x == y) return x;
+        if (-parent_or_size[x] < -parent_or_size[y]) swap(x, y);
+        parent_or_size[x] += parent_or_size[y];
+        parent_or_size[y] = x;
+        return x;
+    }
+
+    bool same(int a, int b) {
+        assert(0 <= a && a < _n);
+        assert(0 <= b && b < _n);
+        return leader(a) == leader(b);
+    }
+
+    int leader(int a) {
+        assert(0 <= a && a < _n);
+        if (parent_or_size[a] < 0) return a;
+        return parent_or_size[a] = leader(parent_or_size[a]);
+    }
+
+    int size(int a) {
+        assert(0 <= a && a < _n);
+        return -parent_or_size[leader(a)];
+    }
+
+    vector<vector<int>> groups() {
+        vector<int> leader_buf(_n), group_size(_n);
+        for (int i = 0; i < _n; i++) {
+            leader_buf[i] = leader(i);
+            group_size[leader_buf[i]]++;
+        }
+        vector<vector<int>> result(_n);
+        for (int i = 0; i < _n; i++) {
+            result[i].reserve(group_size[i]);
+        }
+        for (int i = 0; i < _n; i++) {
+            result[leader_buf[i]].push_back(i);
+        }
+        result.erase(
+            remove_if(result.begin(), result.end(),
+                           [&](const vector<int>& v) { return v.empty(); }),
+            result.end());
+        return result;
+    }
+
+  private:
+    int _n;
+    // root node: -1 * component size
+    // otherwise: parent
+vector<int> parent_or_size;
+};
+
+int main()
+{
+   #ifndef ONLINE_JUDGE
+    freopen("inputa.txt","r",stdin);
+    freopen("outputa.txt","w",stdout);
+    freopen("log.txt","w",stderr);
+    #endif
+int t;
+t = 1;
+//cin>>t;
+while(t--){
+    ll n,m;
+    cin>>n>>m;
+    dsu d(n);
+    while(m--){
+        ll ti ;
+        cin>>ti;
+        ll a,b;
+        cin>>a>>b;
+        if(ti==0){
+            d.merge(a,b);
+
+        }
+        else{
+            if(d.same(a,b))
+                cout<<1<<endl;
+            else
+                cout<<0<<endl;
+
+        }
+    }
+
+}
+Time
+}
